b08274c2436bee4402d32d2ebbd95db2c604c639
Hugo van Kemenade <hugovk@users.noreply.github.com>
2020-12-14 20:51:31 +0200

    Drop support for EOL Python 3.4

--- setup.py
+++ setup.py
@@ -142,7 +142,6 @@ def main():
                        'Intended Audience :: Developers',
                        'Programming Language :: Python',
                        'Programming Language :: Python :: 3',
-                       'Programming Language :: Python :: 3.4',
                        'Programming Language :: Python :: 3.5',
                        'Programming Language :: Python :: 3.6',
                        'Programming Language :: Python :: 3.7',
